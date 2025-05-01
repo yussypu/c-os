956fbd5a9335f9c15da7b20defa5a047cb526c77
Yahya <m.y.b.e.qureshi@student.vu.nl>
2025-05-01 12:56:51 +0200

    Initial working kernel with printf and SBI support

---
+++ common.c
@@ -0,0 +1,64 @@
+#include "common.h"
+
+void putchar(char ch);
+
+void printf(const char *fmt, ...) {
+    va_list vargs;
+    va_start(vargs, fmt);
+
+    while (*fmt) {
+        if (*fmt == '%') {
+            fmt++; // Skip '%'
+            switch (*fmt) { // Read the next character
+                case '\0': // '%' at the end of the format string
+                    putchar('%');
+                    goto end;
+                case '%': // Print '%'
+                    putchar('%');
+                    break;
+                case 's': { // Print a NULL-terminated string.
+                    const char *s = va_arg(vargs, const char *);
+                    while (*s) {
+                        putchar(*s);
+                        s++;
+                    }
+                    break;
+                }
+                case 'd': { // Print an integer in decimal.
+                    int value = va_arg(vargs, int);
+                    unsigned magnitude = value; // https://github.com/nuta/operating-system-in-1000-lines/issues/64
+                    if (value < 0) {
+                        putchar('-');
+                        magnitude = -magnitude;
+                    }
+
+                    unsigned divisor = 1;
+                    while (magnitude / divisor > 9)
+                        divisor *= 10;
+
+                    while (divisor > 0) {
+                        putchar('0' + magnitude / divisor);
+                        magnitude %= divisor;
+                        divisor /= 10;
+                    }
+
+                    break;
+                }
+                case 'x': { // Print an integer in hexadecimal.
+                    unsigned value = va_arg(vargs, unsigned);
+                    for (int i = 7; i >= 0; i--) {
+                        unsigned nibble = (value >> (i * 4)) & 0xf;
+                        putchar("0123456789abcdef"[nibble]);
+                    }
+                }
+            }
+        } else {
+            putchar(*fmt);
+        }
+
+        fmt++;
+    }
+
+end:
+    va_end(vargs);
+}
---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -xue
+
+QEMU=qemu-system-riscv32
+
+# Path to clang and compiler flags
+CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+
+# Build the kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c common.c
+
+# Start QEMU
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
